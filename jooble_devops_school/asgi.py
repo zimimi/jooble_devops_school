b838f70202005f489e44fd86d63e5b0d337a62f0
Dimka Kushchevskyi <dima.kushhevskij@gmail.com>
2021-09-29 06:42:30 +0300

    Initial commit

---
+++ jooble_devops_school/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jooble_devops_school project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jooble_devops_school.settings')
+
+application = get_asgi_application()
---
+++ jooble_devops_school/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jooble_devops_school project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jooble_devops_school.settings')
+
+application = get_wsgi_application()

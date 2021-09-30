3c73c0c368320146946d5bcec31d506f178215e2
Dimka Kushchevskyi <dima.kushhevskij@gmail.com>
2021-09-30 07:24:00 +0300

    Add more fields in db

--- crud_app/models.py
+++ crud_app/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Film(models.Model):  
     name = models.CharField(max_length=100)
-    rating = models.CharField(max_length=20)
+    kinopoisk_rating = models.FloatField()
+    IMDb_rating = models.FloatField()
+    RT_rating = models.IntegerField()
+    year = models.IntegerField()
+    duration = models.CharField(max_length=100)
 
     class Meta:
         db_table = "film_library"
